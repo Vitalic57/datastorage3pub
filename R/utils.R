f9187fd73b43949b6483dab97780fec320a3268a
Vitaly <vitalic57@bk.ru>
2021-12-09 17:44:53 +0300

    get_subset

--- R/utils.R
+++ R/utils.R
@@ -34,3 +34,47 @@ shrink_by_ind <- function(data, start_ind, end_ind, copy=TRUE){
 
   return(invisible(res))
 }
+
+
+
+#' Get subset of Data object
+#' 
+#' All tables will be subset
+#'
+#' @param data Data
+#' @param rows numeric vector
+#' @param columns character vector
+#'
+#' @return Data
+#' @export
+get_subset <- function(data, rows, columns){
+  if(missing(rows)){
+    rows <- 1:data[['nrow']]
+  }
+  if(missing(columns)){
+    columns <- data[['colnames']]
+  }
+  if(is.numeric(columns)){
+    columns <- data$colnames[columns]
+  }
+  res <- new.env()
+  class(res) <- c("Data", "environment")
+  for(name in data$price_columns){
+    res[[name]] <- data[[name]][rows, columns, drop=FALSE]
+  }
+  res[["indexes"]] <- list()
+  for(name in names(data[["indexes"]])){
+    res[["indexes"]][[name]] <- data[["indexes"]][[name]][rows, columns, drop=FALSE]
+  }
+  for(name in names(data)){
+    if(name %in% c( 'dates', 'indexes', 'colnames', data[['price_columns']])){
+      next
+    }
+    res[[name]] <- data[[name]]
+  }
+  res[['dates']] <- data[['dates']][rows]
+  res[['colnames']] <- columns
+  res[['ncol']] <- length(columns)
+  res[['nrow']] <- length(rows)
+  return(invisible(res))
+}
